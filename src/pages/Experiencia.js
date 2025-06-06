f6c6a8bc6f395f3b32d9a09ed20d5244316fa3d1
WhiteCollins <collinsjcwhite@gmail.com>
2025-06-06 09:07:22 -0400

    Completada la página de Experiencia con texto descriptivo y video incrustado

--- src/pages/Experiencia.js
+++ src/pages/Experiencia.js
@@ -11,7 +11,7 @@ function Experiencia() {
       </Typography>
       
       <Typography variant="body1" sx={{ mb: 2 }}>
-       
+        En este proyecto he utilizado diversas tecnologías modernas para el desarrollo de aplicaciones móviles. Principalmente, he trabajado con <b>React.js</b> como biblioteca frontend y <b>Material UI</b> para los componentes de interfaz de usuario. La aplicación se ha adaptado para dispositivos Android mediante <b>Capacitor</b>, que permite convertir aplicaciones web en aplicaciones móviles nativas.
       </Typography>
       
       <Box sx={{ 
@@ -28,7 +28,7 @@ function Experiencia() {
           width="100%"
           height="100%"
           style={{ position: 'absolute', top: 0, left: 0 }}
-          src="https://youtu.be/Zi_XLOBDo_Y?si=1pQb2Rrbz1bLdbt1"
+          src="https://www.youtube.com/embed/Zi_XLOBDo_Y"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
@@ -38,7 +38,7 @@ function Experiencia() {
       <Divider sx={{ my: 2 }} />
       
       <Typography variant="body2" sx={{ fontStyle: 'italic' }}>
-        
+        El desarrollo de esta aplicación me ha permitido aplicar conocimientos de programación React y diseño de interfaces modernas. Ha sido un desafío integrar todas las funcionalidades manteniendo una interfaz amigable y adaptable a dispositivos móviles. Lo más interesante ha sido la implementación de la navegación entre páginas y la gestión de estados en los componentes.
       </Typography>
     </Paper>
   );
